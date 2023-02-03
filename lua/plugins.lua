75037ce377414c4d44e293c4d727df1d8969c2bd
GianK128 <giankeberlein@gmail.com>
2023-02-03 16:13:03 -0300

    Add treesitter plugin and configuration file

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -20,4 +20,11 @@ packer.startup(function(use)
     use 'hrsh7th/nvim-cmp' -- Completion
     use 'neovim/nvim-lspconfig' -- LSP
     use 'L3MON4D3/LuaSnip' -- Snippet engine
+    use {
+        'nvim-treesitter/nvim-treesitter',
+        run = function()
+            local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
+            ts_update()
+        end,
+    }
 end)
