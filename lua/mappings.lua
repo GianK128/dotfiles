42445e4831ed80a9d7ae392eea881a7768a39b47
GianK128 <giankeberlein@gmail.com>
2023-02-09 09:42:17 -0300

    Add save and move lines mappings

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -3,3 +3,10 @@ local keymap = vim.keymap
 -- Moving lines up and down
 keymap.set('n', '<A-Up>', ':m -2<CR>')
 keymap.set('n', '<A-Down>', ':m +1<CR>')
+
+keymap.set('i', '<A-Up>', '<Esc>:m -2<CR>')
+keymap.set('i', '<A-Down>', '<Esc>:m +1<CR>')
+
+-- Save with different command
+keymap.set('n', '<C-s>', ':w<CR>')
+keymap.set('i', '<C-s>', '<Esc>:w<CR>')
