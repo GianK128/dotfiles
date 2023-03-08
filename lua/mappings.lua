5fc9dcc07cd87ef02778052fba7cdbf2a9ee7eb9
GianK128 <giankeberlein@gmail.com>
2023-03-08 09:35:44 -0300

    Add mappings to move lines in visual moder

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -7,6 +7,9 @@ keymap.set('n', '<A-Down>', ':m +1<CR>')
 keymap.set('i', '<A-Up>', '<Esc>:m -2<CR>')
 keymap.set('i', '<A-Down>', '<Esc>:m +1<CR>')
 
+keymap.set('v', '<A-Up>', '<Esc>:m -2<CR>')
+keymap.set('v', '<A-Down>', '<Esc>:m +1<CR>')
+
 -- Save with different command
 keymap.set('n', '<C-s>', ':w<CR>')
 keymap.set('i', '<C-s>', '<Esc>:w<CR>')
