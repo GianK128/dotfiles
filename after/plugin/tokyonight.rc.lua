6e6fbfeed3bbd84284bc1e998aceca739d759b8d
GianK128 <giankeberlein@gmail.com>
2023-03-08 13:44:38 -0300

    Make window transparent and change variant

--- after/plugin/tokyonight.rc.lua
+++ after/plugin/tokyonight.rc.lua
@@ -4,9 +4,9 @@ if (not status) then return end
 cs.setup({
     -- your configuration comes here
     -- or leave it empty to use the default settings
-    style = "night", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
+    style = "storm", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
     light_style = "day", -- The theme is used when the background is set to light
-    transparent = false, -- Enable this to disable setting the background color
+    transparent = true, -- Enable this to disable setting the background color
     terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
     styles = {
         -- Style to be applied to different syntax groups
